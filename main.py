0211b7c4c5bcc3a4bdc89654a60fd93bf9046542
Kaobratt <101005257+Kaobratt@users.noreply.github.com>
2023-08-11 15:13:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ def main():
         seven_minute = now.minute
 
         # 1.整點10分出前後3小時
-        if minute == 10:
-        # if True:
+        # if minute == 10:
+        if True:
             today_date = datetime.datetime.now().date().strftime('%Y_%m_%d')
             file_name = f'{today_date}_update.xls'
             response = requests.get(f'https://www.taoyuan-airport.com/uploads/fos/{file_name}')
@@ -107,7 +107,9 @@ def main():
                 plt.close()
 
                 Notify('整點10分-提醒', 'data1_table.png')
-                time.sleep(2340)
+                print('完成休息')
+                # time.sleep(2340)
+                time.sleep(60)
 
         # 2.晚上7:00出明天的
         if seven_hour == 19 and seven_minute in [0, 1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 ,19, 20]:
